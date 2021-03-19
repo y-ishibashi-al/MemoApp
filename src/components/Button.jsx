0d2b531efb73005e8cb60fecd56adbcf18ecbd8b
y-ishibashi <80504126+y-ishibashi-al@users.noreply.github.com>
2021-03-19 17:13:19 +0900

    Make components touchable

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,19 +1,24 @@
-import { string } from 'prop-types';
+import { func, string } from 'prop-types';
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default function Button(props){
-    const { label } = props;
+    const { label, onPress } = props;
     return (
-        <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.buttonContainer} onPress={onPress}>
             <Text style={styles.buttonLabel}>{label}</Text>
-        </View>
+        </TouchableOpacity>
     );
 }
 
 Button.propTypes = {
     label: string.isRequired,
-}
+    onPress: func,
+};
+
+Button.defaultProps = {
+    onPress: null,
+};
 
 const styles = StyleSheet.create({
     buttonContainer:{
--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, TextInput,  } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity  } from 'react-native';
 
 import AppBar from '../components/AppBar';
 import Button from '../components/Button';
@@ -12,10 +12,13 @@ export default function SignUpScreen(){
                 <Text style={styles.title}>Sign Up</Text>
                 <TextInput style={styles.input} value="Email Address"></TextInput>
                 <TextInput style={styles.input} value="Password"></TextInput>
-                <Button label='Submit'/>
+                <Button label='Submit' onPress={() => {Alert.alert("Submit!")}}/>
                 <View style={styles.footer}>
                     <Text style={styles.footerText}>Already registered?</Text>
-                    <Text style={styles.footerLink}>Log In.</Text>
+                    <TouchableOpacity>
+                         <Text style={styles.footerLink}>Log In.</Text>
+                    </TouchableOpacity>
+
                 </View>
             </View>
         </View>
