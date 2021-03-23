f0eb9b359fdd56f724e42ce463d2402f3da5aa35
y-ishibashi <80504126+y-ishibashi-al@users.noreply.github.com>
2021-03-23 15:13:50 +0900

    Fetch data from firestore and pass it to FlatList

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -1,32 +1,57 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Alert, FlatList } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import { arrayOf, instanceOf, shape, string } from 'prop-types';
 
-export default function MemoList(){
+export default function MemoList(props){
+    const { memos } = props;
     const navigation = useNavigation();
-    return (
-        <View>
+
+    function renderItem({item}){
+        return (
             <TouchableOpacity
                 style={styles.memoListItem}
-                onPress={() => { navigation.navigate('MemoDetail')  }}
+                onPress={() => { navigation.navigate('MemoDetail');  }}
             >
                 <View>
-                    <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-                    <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
+                    <Text style={styles.memoListItemTitle} numberOfLines={1}>{memo.bodyText}</Text>
+                    <Text style={styles.memoListItemDate}>{String(memo.updatedAt)}</Text>
                 </View>
                 <TouchableOpacity
                     style={styles.memoDelete}
-                    onPress={() => { Alert.alert('Are you sure?'); }}>
+                    onPress={() => { Alert.alert('Are you sure?'); }}
+                >
                     <Feather name="x" size={16} color="#B0B0B0" />
                 </TouchableOpacity>
             </TouchableOpacity>
+        );
+    }
+
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={memos}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.id}
+            />
         </View>
 
     );
 }
 
+MemoList.propTypes = {
+    memos: arrayOf(shape({
+        id: string,
+        bodyText: string,
+        updatedAt: instanceOf(Date),
+    })).isRequired,
+}
+
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
     memoListItem: {
         backgroundColor: '#ffffff',
         flexDirection: 'row',
