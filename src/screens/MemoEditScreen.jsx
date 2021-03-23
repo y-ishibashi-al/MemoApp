33535efcc3a044cb4f3e4cc8bff6d98c9c7b4a08
y-ishibashi <80504126+y-ishibashi-al@users.noreply.github.com>
2021-03-23 16:36:03 +0900

    Update memo data on MemoEditScreen

--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -1,22 +1,58 @@
-import React from 'react';
-import { View, StyleSheet, TextInput, KeyboardAvoidingView, } from 'react-native';
-
+import React, { useState } from 'react';
+import { View, StyleSheet, TextInput, KeyboardAvoidingView, Alert, } from 'react-native';
+import firebase from 'firebase';
 import CircleButton from '../components/CircleButton';
+import {shape,string} from 'prop-types';
 
 export default function MemoEditScreen(props){
-    const { navigation } = props;
+    const { navigation, route } = props;
+    const { id, bodyText } = route.params;
+    const [body, setBody] = useState(bodyText);
+
+    function handlePress(){
+        const { currentUser } = firebase.auth();
+        if(currentUser){
+            const db = firebase.firestore();
+            const ref = db.collection(`users/${currentUser.uid}/memos`.doc(id));
+            ref.set({
+                bodyText: body,
+                updatedAt: new Date(),
+            }, {merge: true})
+             .then(() => {
+                navigation.goBack();
+             })
+             .catch((error) => {
+                 Alert.alert(error.code);
+             });
+        }
+    }
+
     return (
         <KeyboardAvoidingView style={styles.container} behavior="height">
             <View style={styles.inputContainer}>
-                <TextInput value="買い物リスト" multiline style={styles.input} />
+                <TextInput
+                    value={body}
+                    multiline
+                    style={styles.input}
+                    onChangeText={(text) => { setBody(text); }}
+                />
             </View>
             <CircleButton
                 name="check"
-                onPress={ () => { navigation.goBack(); }}
+                onPress={handlePress}
             />
         </KeyboardAvoidingView>
     )
 }
+
+MemoEditScreen.propTypes = {
+    route: shape({
+        params: shape({
+            id: string,
+            bodyText: string,
+        }),
+    }).isRequired,
+};
 
 const styles = StyleSheet.create({
     container: {
