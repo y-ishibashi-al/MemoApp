d1c6a77cc3a5258b7b56bc21b22b24094f5636aa
y-ishibashi <80504126+y-ishibashi-al@users.noreply.github.com>
2021-03-23 14:12:37 +0900

    Save memo on firestroe

--- src/screens/MemoCreateScreen.jsx
+++ src/screens/MemoCreateScreen.jsx
@@ -1,19 +1,46 @@
 import { NavigationHelpersContext } from '@react-navigation/core';
-import React from 'react';
+import React, {useState, } from 'react';
 import { View, StyleSheet, TextInput,KeyboardAvoidingView } from 'react-native';
+
+import firebase from "firebase";
 
 import CircleButton from '../components/CircleButton';
 
 export default function MemoCreateScreen(props){
     const { navigation } = props;
+    const [bodyText, setBodyText] = useState('');
+
+    function handlePress(){
+        const { currentUser } = firebase.auth();
+        const db = firebase.firestore();
+        const ref = db.collection(`users/${currentUser.uid}/memos`);
+        ref.add({
+            bodyText,
+            updatedAt: new Date(),
+        })
+         .then((docRef) => {
+             console.log('Created', docRef.id);
+             navigation.goBack();
+         })
+         .catch((error) => {
+             console.log('Error!', error)
+         });
+    }
+
     return (
         <KeyboardAvoidingView style={styles.container} behavior="height">
             <View style={styles.inputContainer}>
-                <TextInput value="" multiline style={styles.input} />
+                <TextInput
+                    value={bodyText}
+                    multiline
+                    style={styles.input}
+                    onChangeText={(text) => { setBodyText(text); }}
+                    autoFocus
+                />
             </View>
             <CircleButton
                 name="check"
-                onPress={ () => { navigation.goBack(); }}
+                onPress={handlePress}
             />
         </KeyboardAvoidingView>
     )
